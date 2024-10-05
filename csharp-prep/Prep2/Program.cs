cc717714fab17086fd19f39ef1ead4dacba0f0a1
TiagoWurmli <ti.wurmli@hotmail.com>
2024-10-05 13:23:33 -0300

    completed Presps

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeString);
+        
+        if (gradePercentage >= 90) 
+        {
+            gradeString = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            gradeString = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            gradeString = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            gradeString = "D";
+        }
+
+        else
+        {
+            gradeString = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeString}.");
+
+        if (gradeString == "A" || gradeString == "B" || gradeString == "C")
+        {
+            Console.WriteLine("You Pass!");
+        }
+
+        else
+        {
+            Console.WriteLine("You fail, good luck next time, you will need it!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine("What is the magic number?");
+
+        int response;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            response = int.Parse(userInput);
+
+            if (number < response)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (number > response)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (response != number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
